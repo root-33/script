79d979f98fa1dd30ba60b9aa3bfca55734e3dc22
root-33 <mem37mm@gmail.com>
2025-11-22 17:52:10 +0300

    Create nj.js

---
+++ nj.js
@@ -0,0 +1,16 @@
+$(`<center><div id="asim1">
+<a id="asim7"rel="dofollow" title="القوانين" href="https://iraqna.net/rules.html"style="border-radius: 0 4px 0 0; border-right: 2px solid#f7d322;"class="fl mini  fa fa-bullhorn btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="الاشتراكات" href="https://iraqna.net/sub.html"style="border-right: 1px solid#fff;"class="fl mini  fa fa-star-half-o btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="راديو" href="https://iraqna.net/radioiraqna.html"style="border-right: 1px solid#fff;"class="fl mini  fa fa-volume-up btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="زخرفة اسماء" href="https://blog.iraqna.net/z"style="border-right: 1px solid#fff;"class="fl mini  fa fa-gg btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="اتصل بنا" href="https://iraqna.net/contact.html"style="border-right: 1px solid#fff;"class="fl mini fa fa-dot-circle-o btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="قوانين السوابر" href="https://iraqna.net/ruless.html"style="border-right: 1px solid#fff;"class="fl mini fa fa-exclamation-triangle btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="تطبيق الشات" href="https://iraqna.net/mobile.html"style="border-right: 1px solid#fff;"class="fl mini fa fa-android btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="اختصارات الشات" href="https://iraqna.net/Shortcut.html"style="border-radius: 6px 0 0 0; border-left: 3px solid#f7d322; border-right: 1px solid#ffffff;"class="fl mini fa fa-tencent-weibo btn btn-primary" target="_blank"></a>
+</div></div><center>`).insertBefore('div#tlogins .lonline.light.break');
+$(`<center><div><marquee direction="right" id="as43" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"> &nbsp;أهلا وسهلا بكم في شات <font color="#595757" class="asim12">نجمه الحب </font> ● · ‫أفضل سوبر لهذا الأسبوع · ● <font class="asim11" color="#727A54">&nbsp; قريبا1&nbsp;</font> ● · ‫أفضل زائر لهذا الأسبوع · ● <font class="asim11" color="#ff9900">&nbsp;قريبا2&nbsp;</font> ● · ‫أفضل زائرة لهذا الأسبوع · ● <font class="asim11" color="#ff00ff">&nbsp; قريبا3&nbsp;</font> ● · مبدع الحائط لهذا الأسبوع · ● <font class="asim11" color="#5CAD9A">&nbsp; قريبا4&nbsp;</font> ● · مبدعة الحائط لهذا الأسبوع · ● <font class="asim11" color="#800000">&nbsp; قريبا5&nbsp;</font> ● · ‫أفضل تواجد لهذا الأسبوع · ● <font class="asim11" color="#417570">&nbsp; قريبا6&nbsp;</font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp;
+</marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break');
+$(`<link rel="stylesheet" href="https://raw.githack.com/Asimiraq/IQ3/main/q.css"></style>`).insertBefore("body");
+$.getScript("https://raw.githack.com/Asimiraq/IQ3/main/coler.js");
+$.getScript("https://raw.githack.com/Asimiraq/IQ3/main/welcome-message.js");
+$.getScript("https://raw.githack.com/Asimiraq/IQ3/main/musi3c.js");
